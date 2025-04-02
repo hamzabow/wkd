be9032d08794ac52f880f7df92eb590e9c133cb3
Hamza Boukhentiche <hamza.boukhentiche@outlook.com>
2025-04-02 21:17:43 +0100

    refactor(src): relocate and update import paths for words module
    The words module has been moved to a higher directory level for better organization and future scalability. Updated import paths accordingly.

--- src/display-nodes.ts
+++ src/display-nodes.ts
@@ -1,5 +1,5 @@
 import config from '../config.ts'
-import { Word } from './config/words.ts'
+import { Word } from './words.ts'
 import { paintBlue, paintGray, paintGreen, paintYellow } from './paint.ts'
 import { Action, Node } from './types/types.ts'
 import { sleep } from './util.ts'
--- src/words.ts
+++ src/words.ts
@@ -1,5 +1,5 @@
-import { UnionToTuple } from '../types/ts-utils.ts'
-import config from '../../config.ts'
+import { UnionToTuple } from './types/ts-utils.ts'
+import config from '../config.ts'
 
 export type Word = keyof typeof config.nodes
 
